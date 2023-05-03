023240d9a6f85c814cffb2ca1a389a94db0ada4e
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-05-03 11:51:47 +0100

    no go to products

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -49,7 +49,7 @@ const router = createRouter({
           },
           children: [
             {
-              path: 'products',
+              path: '/',
               component: Products,
               meta: {
                 view: FooterViews.PROJECT
