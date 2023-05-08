a4d34958869a68f70ff2007802e00980f06e920c
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-05-08 17:14:48 +0100

    fix on line chart

--- front/src/components/project/BadLineChart.vue
+++ front/src/components/project/BadLineChart.vue
@@ -1,68 +1,101 @@
 <template>
     <Line
-      :id="chartId"
-      :data="lineData"
-      :options="lineOptions"
+		:id="chartId"
+		:data="lineData"
+		:options="options"
     />
-  </template>
+</template>
   
-  <script>
-  import { Line, Chart } from 'vue-chartjs'
-  
-  
-  export default {
-    name: 'BadLineChart',
-    components: {
-      Line
-    },
-    props: {
-      chartId: {
-        type: String,
-        default: 'my-line-id'
-      },
-      labels: {
-        type: Array,
-        default: () => []
-      },
-      datasets: {
-        type: Array,
-        default: () => []
-      },
-      totalBudget: {
-        type: Number,
-        default: 0
-      }
-    },
-    data() {
-      return {
-        lineData: {
-          labels: this.labels,
-          datasets: this.datasets
-        },
-        lineOptions: {
-          responsive: true,
-          scales: {
-            x: {
-              ticks: {
-                beginAtZero: true
-              }
-            },
-            y: {
-              ticks: {
-                beginAtZero: true,
-                max: this.totalBudget,
-                stepSize: 1000
-              },
-              grid: {
-                display: true,
-                color: 'black',
-                borderDash: [2, 5],
-                lineWidth: 2
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  </script>
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line, Chart } from 'vue-chartjs'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+
+export default {
+	name: 'BadLineChart',
+	components: {
+		Line
+	},
+	props: {
+		chartId: {
+			type: String,
+			default: 'my-line-id'
+		},
+		labels: {
+			type: Array,
+			default: () => []
+		},
+		datasets: {
+			type: Array,
+			default: () => []
+		},
+		totalBudget: {
+			type: Number,
+			default: 0
+		}
+	},
+	data() {
+		return {
+			lineData: {
+				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+				datasets: [
+						{
+							label: 'Data One',
+							backgroundColor: '#f87979',
+							data: [40, 39, 10, 40, 39, 80, 40]
+						}
+					]
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false
+			},
+			lineOptions: {
+				responsive: true,
+				scales: {
+				x: {
+					ticks: {
+					beginAtZero: true
+					}
+				},
+				y: {
+					ticks: {
+						beginAtZero: true,
+						max: this.totalBudget,
+						stepSize: 1000
+					},
+					grid: {
+						display: true,
+						color: 'black',
+						borderDash: [2, 5],
+						lineWidth: 2
+					}
+				}
+				}
+			}
+		}
+	},
+	created() {
+		console.log(this)
+	}
+}
+</script>
