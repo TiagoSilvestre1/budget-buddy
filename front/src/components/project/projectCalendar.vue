54aec841db26f3d9ae2f637db9bdf98cc896f52d
Tiago Silvestre <tiago.silvestre@ua.pt>
2023-05-09 23:51:18 +0100

    bug fixes on calendar

--- front/src/components/project/projectCalendar.vue
+++ front/src/components/project/projectCalendar.vue
@@ -29,120 +29,121 @@ export default {
   methods: {
     async generate_original_attribs()
     {
-      let arr: any[] = [];
-      await backendService.get('api/project/byUserId?user_id=' + this.getUser.id).then((response: API) => {
-      if('success' in response && response.success === true)
-      {
-        this.projects = response.data.owned.concat(response.data.collaborates);
-        this.projects.forEach((project: Project) => {
-          let dates: any;
-          let popover: {
-            label: string;
-            visibility: 'focus'
-          } = {label: '' , visibility: 'focus'};
-          let highlight: any = {
+        let arr: any[] = [];
+  const response: API = await backendService.get('api/project/byUserId?user_id=' + this.getUser.id);
+  if ('success' in response && response.success === true) {
+    this.projects = response.data.owned.concat(response.data.collaborates);
+    for (const project of this.projects) {
+      let dates: any;
+      let popover: {
+        label: string;
+        visibility: 'focus'
+      } = { label: '', visibility: 'focus' };
+      let highlight: any = {};
 
-          };
+      let content = null;
+      let dot = null;
 
-          let content = null;
-          let dot = null;
-
-          if(project.start_date && project.finish_date)
-          {
-            dates = { start: project.start_date, end: project.finish_date};
-            popover.label = 'Project ' + project.title;
-            
-
-          }
-
-          if(project.start_date && !project.finish_date)
-          {
-            dates = project.start_date;
-            content = 'green'
-            highlight = {
-            color: 'green',
-            fillMode: 'outline',
-          }
-            dot = true
-            popover.label = 'Project ' + project.title + " start date";  
-          }
-
-          if(!project.start_date && project.finish_date)
-          {
-            dates = project.finish_date;
-            dot = true
-            popover.label = 'Project ' + project.title + ' end date';
-          }
-         
-            arr.push({
-              highlight: highlight,
-              popover: popover,
-              dates: dates,
-              content: content,
-              dot: dot,
-              title: project.title
-            });
-            
-            project.products.forEach((prod_id: string) => {backendService.get('api/product/productById?id=' + prod_id, true).then((res: AxiosResponse) => {
-              if(res.status === HttpStatusCode.Ok)
-              {
-                let product: Product = res.data;
-
-                product.quotes.forEach((el: string) => {
-                  backendService.get('api/product/quoteById?id=' + el, true).then((quote_res: AxiosResponse) => {
-
-                    if(quote_res.status === HttpStatusCode.Ok)
-                    {
-                      let quote: Quote = quote_res.data;
-
-                      if(product.product)
-                      {
-                        arr.push({
-                          highlight: highlight,
-                          popover: 
-                          {
-                            label: product.name + " from " + quote.url + " costing " + quote.price + "€ if purchased, arrives",
-                            visibility: 'focus'
-                          },
-                          dot: null,
-                          content: 'red',
-                          title: project.title,
-                          dates: quote.available
-                        })
-                      }
-                      else{
-                        let quote_dates = null;
-                        if(quote.available && quote.available_2)
-                          quote_dates = {start: quote.available, end: quote.available_2};
-                        if(quote.available && !quote.available_2)
-                          quote_dates = quote.available;
-                        if(!quote.available && quote.available_2)
-                          quote_dates = quote.available_2;
-
-                        arr.push({
-                          highlight: highlight,
-                          popover: 
-                          {
-                            label: "Service " + product.name + " occurs",
-                            visibility: 'focus'
-                          },
-                          dot: null,
-                          content: 'orange',
-                          title: project.title,
-                          dates: quote_dates
-                        })
-                      }
-                    }
-                  });
-                });
-              }
-            })});
-          });
-
-          
+      if (project.start_date && project.finish_date) {
+        dates = { start: project.start_date, end: project.finish_date };
+        popover.label = 'Project ' + project.title;
       }
-    });
-    return arr;
+
+      if (project.start_date && !project.finish_date) {
+        dates = project.start_date;
+        content = 'green';
+        highlight = {
+          color: 'green',
+          fillMode: 'outline',
+        };
+        dot = true;
+        popover.label = 'Project ' + project.title + " start date";
+      }
+
+      if (!project.start_date && project.finish_date) {
+        dates = project.finish_date;
+        dot = true;
+        popover.label = 'Project ' + project.title + ' end date';
+      }
+
+      arr.push({
+        highlight: highlight,
+        popover: popover,
+        dates: dates,
+        content: content,
+        dot: dot,
+        title: project.title
+      });
+
+      for (const prod_id of project.products) {
+        const res: AxiosResponse = await backendService.get('api/product/productById?id=' + prod_id, true);
+        if (res.status === HttpStatusCode.Ok) {
+          let product: Product = res.data;
+
+          for (const el of product.quotes) {
+            const quote_res: AxiosResponse = await backendService.get('api/product/quoteById?id=' + el, true);
+
+            if (quote_res.status === HttpStatusCode.Ok) {
+              let quote: Quote = quote_res.data;
+
+              if (product.product) {
+                let label_generator = (productName: string, url: any, price: any) => {
+                    let label = productName;
+  
+                if (url) {
+                    label += ` from ${url}`;
+                }
+
+                if(price)
+                {
+                    label += ` costing ${price}`;
+                }
+
+                label += ' if purchased arrives';
+                return label;
+                };
+                arr.push({
+                  highlight: highlight,
+                  popover:
+                    {
+                      label: label_generator(product.name, quote.url, quote.price),
+                      visibility: 'focus'
+                    },
+                  dot: true,
+                  content: 'blue',
+                  title: project.title,
+                  dates: quote.available
+                })
+              }
+              else {
+                let quote_dates = null;
+                if (quote.available && quote.available_2)
+                  quote_dates = { start: quote.available, end: quote.available_2 };
+                if (quote.available && !quote.available_2)
+                  quote_dates = quote.available;
+                if (!quote.available && quote.available_2)
+                  quote_dates = quote.available_2;
+
+                arr.push({
+                  highlight: null,
+                  popover:
+                    {
+                      label: "Service " + product.name + " occurs",
+                      visibility: 'focus'
+                    },
+                  bar: true,
+
+                  title: project.title,
+                  dates: quote_dates
+                })
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return arr;
     },
     filter_selection(arr: any[]){
       this.attributes = [];
